ca8bb4137b2f6fa6963792f3f3b6eee87e72e5a2
gucc1 <swallow965dev@gmail.com>
2019-01-14 15:12:35 +0900

    Plot processing time chainging num of users

--- utm-network/client/results/plot/plot.py
+++ utm-network/client/results/plot/plot.py
@@ -3,12 +3,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
-data = np.loadtxt("../org1-user5-1547384423",
-                  delimiter=' ', skiprows=1, usecols=(0, 2))
-print(data)
+user1 = np.loadtxt("../org1-user1",
+                   delimiter=' ', skiprows=1, usecols=(0, 2))
+user5 = np.loadtxt("../org1-user5",
+                   delimiter=' ', skiprows=1, usecols=(0, 2))
+user10 = np.loadtxt("../org1-user10",
+                    delimiter=' ', skiprows=1, usecols=(0, 2))
+user50 = np.loadtxt("../org1-user50",
+                    delimiter=' ', skiprows=1, usecols=(0, 2))
+user100 = np.loadtxt("../org1-user100",
+                     delimiter=' ', skiprows=1, usecols=(0, 2))
+
+
+# plt.plot(user1[:, 0], user1[:, 1])
+plt.plot(user5[:, 0], user5[:, 1], label='5 users')
+plt.plot(user10[:, 0], user10[:, 1], label='10 users')
+plt.plot(user50[:, 0], user50[:, 1], label='50 users')
+plt.plot(user100[:, 0], user100[:, 1], label='100 users')
+plt.legend()
 
 # 横軸は整数値にする
 plt.gca().xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
 
-plt.plot(data[0:5, 0:1], data[0:5, 1:])
+plt.title('Processing time : 1 node')
+plt.xlim(1, 100)
+plt.xlabel('Number of users')
+plt.ylabel('Processing time(ms)')
+
+# 保存
+# plt.savefig('processingTime-Org1.png')
 plt.show()
