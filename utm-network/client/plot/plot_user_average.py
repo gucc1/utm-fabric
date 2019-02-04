db325211ccb88c179524193a9799f48fcabd14ec
gucc1 <swallow965dev@gmail.com>
2019-02-04 19:03:13 +0900

    Plot ex data

--- utm-network/client/plot/plot_user_average.py
+++ utm-network/client/plot/plot_user_average.py
@@ -3,38 +3,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
-for numOfOrg in range(1, 9):
-    org = "org" + str(numOfOrg)
-    user1 = np.loadtxt("../" + org + "-user1",
-                       delimiter=' ', skiprows=1, usecols=(2))
-    user5 = np.loadtxt("../" + org + "-user5",
-                       delimiter=' ', skiprows=1, usecols=(2))
-    user10 = np.loadtxt("../" + org + "-user10",
-                        delimiter=' ', skiprows=1, usecols=(2))
-    user50 = np.loadtxt("../" + org + "-user50",
-                        delimiter=' ', skiprows=1, usecols=(2))
-    user100 = np.loadtxt("../" + org + "-user100",
-                         delimiter=' ', skiprows=1, usecols=(2))
+for numOfNode in [1, 2, 4, 8, 16, 30]:
+    node = "node" + str(numOfNode)
+    path = "./plot_data/" + node
+    requests = [1, 5, 10, 50, 100, 500, 1000]
+    averages = []
+    for numOfRequest in requests:
 
-    user1_ave = np.mean(user1)
-    user5_ave = np.mean(user5)
-    user10_ave = np.mean(user10)
-    user50_ave = np.mean(user50)
-    user100_ave = np.mean(user100)
+        rawData = np.loadtxt(path + "/user" + str(numOfRequest),
+                        delimiter=' ', skiprows=1, usecols=(1, 2), dtype=str)
+        # user5 = np.loadtxt(path + "/user5",
+        #                 delimiter=' ', skiprows=1, usecols=(2))
+        # user10 = np.loadtxt(path + "/user10",
+        #                 delimiter=' ', skiprows=1, usecols=(2))
+        # user50 = np.loadtxt(path + "/user50",
+        #                 delimiter=' ', skiprows=1, usecols=(2))
+        # user100 = np.loadtxt(path + "/user100",
+        #                 delimiter=' ', skiprows=1, usecols=(2))
+        # user500 = np.loadtxt(path + "/user500",
+        #                 delimiter=' ', skiprows=1, usecols=(2))
+        # user1000 = np.loadtxt(path + "/user1000",
+        #                 delimiter=' ', skiprows=1, usecols=(2))
+
+        successData = []
+        for data in rawData:
+            if data[0] == 'success':
+                successData.append(float(data[1]))
+
+
+        averages.append(np.mean(successData))
+
+        # user1_ave = np.mean(user1)
+        # user5_ave = np.mean(user5)
+        # user10_ave = np.mean(user10)
+        # user50_ave = np.mean(user50)
+        # user100_ave = np.mean(user100)
+        # user500_ave = np.mean(user500)
 
     # plt.plot(user1[:, 0], user1[:, 1])
-    plt.plot([1, 5, 10, 50, 100], [user1_ave, user5_ave,
-                                   user10_ave, user50_ave, user100_ave], label=str(numOfOrg) + ' node')
+    plt.plot(requests, averages, label=str(numOfNode) + ' node')
 
 # 横軸は整数値にする
 plt.gca().xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
 
 plt.title('Average Processing time')
-plt.xlim(1, 100)
-plt.xlabel('Number of users')
-plt.ylabel('Processing time(ms)')
+plt.xlim(1, 1000)
+plt.xlabel('Number of user')
+plt.ylabel('Latency(ms)')
 plt.legend()
 
 # 保存
-plt.savefig('processingTime-node-average.png')
+plt.savefig('test.png')
 plt.show()
