06a620f8620a02aa7c7a9c2df9f1ac2b0c395853
gucc1 <swallow965dev@gmail.com>
2019-02-25 12:06:13 +0900

    Delete unnecessary files

--- utm-network/client/plot/concatData.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for node in 1 2 4 8 16 30; do
-    for user in 1 5 10 50 100 500 1000; do
-        cat "../results/production/node$node/user$user"-* | sed -e "/^id.*/D" > "./plot_data/node$node/user$user"
-    done
-done
